0d962bdc1daa3ae6d0a08f8d87addb9e1bfb9657
ginotas <ginotas@gmail.com>
2021-10-04 12:30:02 +0300

    all done context

--- src/components/Letter.jsx
+++ src/components/Letter.jsx
@@ -1,6 +1,10 @@
+import { useContext } from "react";
+import { ThemeSelect, ThemeContext  } from "../Providers";
 
 export function Letter({letter}) {
+    const th = useContext(ThemeContext);
+    const ts = useContext(ThemeSelect)
     return (
-        <span>{letter}</span>
+        <span style={th[ts]}>{letter}</span>
     )
 }
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,14 +1,51 @@
 import Field from "./Field";
+import { useState } from "react";
+import { ThemeContext, ThemeSelect } from "../Providers";
 
 const letters = ["L", "a", "b", "a", "s"];
 
+ const theme = 
+    [
+    
+        {
+        color: "red",
+        border: "2px solid red"
+    },
+    {
+        color: "green",
+        border: "2px solid green"
+    },
+    {
+        color: "blue",
+        border: "2px solid blue"
+    }
+
+    ]
+;
+
+
+
 function App() {
+
+
+    const [style, setStyle] = useState();
+
+    const changeTheme = t => {
+        setStyle(t);
+    }
+
+
+
     return (
         <>
+        <ThemeContext.Provider value={theme} >
+        <ThemeSelect.Provider value={style}>  
        <Field letters={letters}></Field>
-       <button>Theme 1</button>
-       <button>Theme 2</button>
-       <button>Theme 3</button>
+       <button onClick={() => changeTheme(0)}>Theme 1</button>
+       <button onClick={() => changeTheme(1)}>Theme 2</button>
+       <button onClick={() => changeTheme(2)}>Theme 3</button>
+       </ThemeSelect.Provider>
+       </ThemeContext.Provider>
         </>
         );
     }
